f0fa21b2be05b9bbc5b951cc4c10abb9d46aab79
italogs97 <italogs97@hotmail.com>
2024-11-08 13:24:57 -0300

    conteudo passado em aulas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //estudar
+        //tipos primitivos
+        //classe string que representa texto na aplicação 
+
+        
+        double salarioMinimo = 2.500;
+
+        short numeroCurto = 1;
+        int numeroNormal1 = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal1;     
+        
+        
+        int numero = 42;
+
+        final double VALOR_DE_PI = 3.14;
+
+       
+
+
+        System.out.print(numero);
     }
 }
